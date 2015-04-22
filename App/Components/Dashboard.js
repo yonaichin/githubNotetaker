9320d505492017248b227a707480c356f1ca9818
Naichin <yonaichin@gmail.com>
2015-04-22 22:01:51 +0800

    Create a component using ScrollView

--- App/Components/Dashboard.js
+++ App/Components/Dashboard.js
@@ -1,5 +1,5 @@
 var React = require('react-native');
-
+var Profile = require('./Profile');
 var {
   Text,
   View,
@@ -41,7 +41,11 @@ class Dashboard extends React.Component{
     return obj;
   }
   goToProfile(){
-    console.log('Going to Profile Page');
+    this.props.navigator.push({
+      component: Profile,
+      title: 'Profile Page',
+      passProps: {userInfo: this.props.userInfo}
+    })
   }
   goToRepos(){
     console.log('Going to Repos');
